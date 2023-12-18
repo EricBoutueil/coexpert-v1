862023983baa968ad9ecfdbc2053834199ef6781
Eric Boutueil <e.boutueil@gmail.com>
2023-12-18 11:36:48 +0100

    params set to cloud

--- app/params.py
+++ app/params.py
@@ -1,7 +1,7 @@
 import os
 from os import path
 
-TARGET = "local"
+TARGET = "cloud"
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 PDF_NBER = 2
 PDF_PATH = path.join(os.path.dirname(__file__), 'raw_data', f'{PDF_NBER}_pdf')
